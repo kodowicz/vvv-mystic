040539d4d3a1e723b434d549a5f28afc947e65db
avenria <annakodowicz@gmail.com>
2017-11-16 11:29:26 +0100

    improved header of page

--- javascript/main.js
+++ javascript/main.js
@@ -6,5 +6,20 @@ navButton.addEventListener('click', function() {
   const closeBtn = document.querySelector('.header__nav-button--close-icon');
   header.classList.toggle('nav-opened');
 })
-/*
-*/
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  /* scrolling navigation */
+  window.onscroll = function() {
+      console.log(window.pageYOffset);
+      var nav = document.querySelector('.header');
+      /* made var which gives the position of <main> */
+      if ( window.pageYOffset > 20 /*main*/ ) {
+          nav.classList.add("scrolled");
+      } else {
+          nav.classList.remove("scrolled");
+      }
+  }
+})
